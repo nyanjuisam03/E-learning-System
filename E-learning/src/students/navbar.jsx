576a89d28f0651a171265f04895ebafc570a1709
Sam Nyanjui <148052945+nyanjuisam03@users.noreply.github.com>
2024-04-22 19:48:04 -0800

    seventh commit

--- E-learning/src/students/navbar.jsx
+++ E-learning/src/students/navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { RxAvatar } from "react-icons/rx";
 import { MdAddToPhotos } from "react-icons/md";
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+
 function Navbar() {
+  const navigate=useNavigate()
+  const handleLogout = () => {
+    auth.signOut().then(() => {
+      navigate('/login');
+    });
+  };
   return (
     <div>
       <div className="navbar bg-base-100">
@@ -26,7 +35,7 @@ function Navbar() {
           <ul className="p-2 bg-base-100 rounded-t-none ">
             <li><a>Profile</a></li>
             <li><a><Link to={'/student-settings'}>Settings</Link></a></li>
-            <li><a>Logout</a></li>
+            <li onClick={handleLogout}><a>Logout</a></li>
           </ul>
         </details>
       </li>
