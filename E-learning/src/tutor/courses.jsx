2983d5ffb3a35a44374c9953efd66107a3267c80
Sam Nyanjui <148052945+nyanjuisam03@users.noreply.github.com>
2024-04-25 18:17:03 -0800

    details

--- E-learning/src/tutor/courses.jsx
+++ E-learning/src/tutor/courses.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import {useState,useEffect} from "react"
 const Courses = () => {
   return (
     <div>
-      
+    <h2>This are courses you creates</h2>
     </div>
   )
 }
