a19e46cbbd0be786f13295b81c17ef21ce646c61
Sam Nyanjui <148052945+nyanjuisam03@users.noreply.github.com>
2024-04-27 14:31:45 -0800

    adding the coursee the user can see

--- E-learning/src/students/course.jsx
+++ E-learning/src/students/course.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function Course() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const querySnapshot = await getDocs(collection(db, 'courses'));
+      const coursesData = [];
+      querySnapshot.forEach((doc) => {
+        coursesData.push({ id: doc.id, ...doc.data() });
+      });
+      setCourses(coursesData);
+    };
+
+    fetchCourses();
+  }, []);
   return (
     <div>
-    <h2>Available course</h2>
+    <h2>Which course do you want to join </h2>
+    <ul>
+        {courses.map((course) => (
+          <li key={course.id}>
+            <strong>{course.name}</strong> - {course.description}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
