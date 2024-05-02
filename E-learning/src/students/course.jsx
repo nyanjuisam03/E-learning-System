a5bb3cd6d69986bb86ba452a9379a80ce780ebd0
Sam Nyanjui <148052945+nyanjuisam03@users.noreply.github.com>
2024-05-02 12:10:36 -0800

    addong the skill

--- E-learning/src/students/course.jsx
+++ E-learning/src/students/course.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
+import { PiCookingPot } from "react-icons/pi";
+import { HiOutlinePaintBrush } from "react-icons/hi2";
+import { FaPhotoVideo } from "react-icons/fa";
+import { GiHairStrands } from "react-icons/gi";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
 
 function Course() {
   const [courses, setCourses] = useState([]);
@@ -20,6 +27,47 @@ function Course() {
   return (
     <div>
     <h2>Which course do you want to join </h2>
+    <Splide  options={ {
+    rewind: true,
+    gap   : '18px',
+    perPage: 3,
+  } } >
+  <SplideSlide className='h-40'>
+    <div className="card w-44 h-32 bg-green-500 shadow-xl my-2  text-white" >
+      <div className='mx-12 my-9'>
+      <PiCookingPot className='mx-5 text-2xl'/>
+      <h2>Cooking</h2>
+      </div>
+    </div>
+  </SplideSlide>
+  <SplideSlide>
+    <div className="card w-44 h-32 bg-green-500 shadow-xl my-2  text-white">
+      <div className='mx-12 my-9'>
+      <HiOutlinePaintBrush className='mx-5 text-2xl' />
+    <h2 className='mx-5'>Art</h2>
+      </div>
+    </div>
+  </SplideSlide>
+  <SplideSlide>
+    <div className="card w-44 h-32 bg-green-500 shadow-xl my-2  text-white">
+      <div className='mx-12 my-7'>
+      <FaPhotoVideo className='mx-5 text-2xl' />
+    <h2>Film And Editng</h2>
+      </div>
+    </div>
+  </SplideSlide>
+  <SplideSlide>
+    <div className="card w-44 h-32 bg-green-500 shadow-xl my-2  text-white">
+      <div className='mx-12 my-7'>
+      <GiHairStrands className='mx-5 text-2xl'/>
+    <h2>Beauty and Hairstyles</h2>
+      </div>
+   
+    </div>
+  </SplideSlide>
+  
+</Splide>
+
     <ul>
         {courses.map((course) => (
           <div key={course.id} className="card w-80 bg-base-100 shadow-xl my-4 p-6">
--- E-learning/src/students/studenthome.jsx
+++ E-learning/src/students/studenthome.jsx
@@ -39,30 +39,39 @@ function Studenthome() {
     <Splide  options={ {
     rewind: true,
     gap   : '18px',
-    perPage: 2,
+    perPage: 3,
   } } >
-  <SplideSlide className="card w-96 bg-base-100 shadow-xl ">
-    <div >
-    <PiCookingPot />
+  <SplideSlide className='h-40'>
+    <div className="card w-44 h-32 bg-green-500 shadow-xl my-2  text-white" >
+      <div className='mx-12 my-9'>
+      <PiCookingPot className='mx-5 text-2xl'/>
       <h2>Cooking</h2>
+      </div>
     </div>
   </SplideSlide>
   <SplideSlide>
-    <div>
-    <HiOutlinePaintBrush />
-    <h2>Art</h2>
+    <div className="card w-44 h-32 bg-green-500 shadow-xl my-2  text-white">
+      <div className='mx-12 my-9'>
+      <HiOutlinePaintBrush className='mx-5 text-2xl' />
+    <h2 className='mx-5'>Art</h2>
+      </div>
     </div>
   </SplideSlide>
   <SplideSlide>
-    <div>
-    <FaPhotoVideo />
+    <div className="card w-44 h-32 bg-green-500 shadow-xl my-2  text-white">
+      <div className='mx-12 my-7'>
+      <FaPhotoVideo className='mx-5 text-2xl' />
     <h2>Film And Editng</h2>
+      </div>
     </div>
   </SplideSlide>
   <SplideSlide>
-    <div>
-    <GiHairStrands />
+    <div className="card w-44 h-32 bg-green-500 shadow-xl my-2  text-white">
+      <div className='mx-12 my-7'>
+      <GiHairStrands className='mx-5 text-2xl'/>
     <h2>Beauty and Hairstyles</h2>
+      </div>
+   
     </div>
   </SplideSlide>
   
@@ -73,7 +82,7 @@ function Studenthome() {
           <h2 className="card-title">{course.name}</h2>
           <p className='text-sm'> {course.description}</p>
           <div className='justify-end'>
-          <button className="btn btn-outline btn-primary">Join Now</button>
+          <button className="btn btn-outline btn-success">Join Now</button>
           </div>
           </div>
         ))}
